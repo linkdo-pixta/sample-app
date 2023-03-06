bf3e820ba7dc5fd232bfa31dc27f7de04f078233
linhdo <linh.dohoang@pixta.co.jp>
2023-03-06 10:13:18 +0700

    Implement I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :set_locale
+
+    private
+    def set_locale
+      I18n.locale = extract_locale || I18n.default_locale
+    end
+    
+    def extract_locale
+      parsed_locale = params[:locale]
+      I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    end
 
     def hello
         render plain: "hello world!"
