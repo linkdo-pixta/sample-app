b10a73ad75bdea215b792a1e6bcc9522e027daa3
linhdo <linh.dohoang@pixta.co.jp>
2023-03-09 10:16:36 +0700

    Chapter 11 - add email activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index ,:edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
-  
+
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user 
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to @user
     else
       render 'new'
     end
@@ -54,9 +54,9 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
-      store_location
+    store_location
       flash[:danger] = "Please log in."
-      redirect_to login_url
+    redirect_to login_url
     end
   end
 
@@ -64,8 +64,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-  
+
   def admin_user
     redirect_to(root_url) unless current_user.admin?
-    end
+  end
 end
