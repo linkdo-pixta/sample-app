f050f7e7fca09ba949b80b87fcc6f02f0b4fb136
linhdo <linh.dohoang@pixta.co.jp>
2023-03-13 09:09:51 +0700

    Chapter 14 - Following user and Feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,26 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: :'Relationship', foreign_key: :followed_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
-  before_create :create_activation_digest 
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_blank: true
   has_secure_password
 
   def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
   def User.new_token
@@ -37,16 +41,18 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_sent_at: Time.zone.now,reset_digest: User.digest(reset_token))
+    update_columns(reset_sent_at: Time.zone.now, reset_digest: User.digest(reset_token))
   end
- 
+
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id ", user_id: id)
   end
 
   def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(attribute, token)
@@ -57,21 +63,33 @@ class User < ApplicationRecord
   end
 
   def forget
-      update_attribute(:remember_digest, nil)
+    update_attribute(:remember_digest, nil)
   end
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user)&.destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
-    def downcase_email
-      self.email = email.downcase
-    end
+  def downcase_email
+    self.email = email.downcase
+  end
 
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
